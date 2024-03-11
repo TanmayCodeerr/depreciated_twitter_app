bd594bc877313a7dc2066be7e98ff3ff1ca743f7
TanmayCodeerr <125294567+TanmayCodeerr@users.noreply.github.com>
2024-03-11 21:00:09 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './components_2.css'
+import './Header.css'
 
 import {  } from '@fortawesome/free-brands-svg-icons'
 import {  } from '@fortawesome/free-regular-svg-icons'
